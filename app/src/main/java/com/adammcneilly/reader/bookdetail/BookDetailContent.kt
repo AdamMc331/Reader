d4c2c471d2815338acf61be29fb4c7b327c76a02
Adam McNeilly <amcneilly331@gmail.com>
2023-01-29 00:51:41 -0500

    Applying color filter to icon.

--- app/src/main/java/com/adammcneilly/reader/bookdetail/BookDetailContent.kt
+++ app/src/main/java/com/adammcneilly/reader/bookdetail/BookDetailContent.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -34,6 +36,7 @@ fun BookDetailContent(
             painter = painterResource(id = R.drawable.baseline_book_24),
             contentDescription = stringResource(R.string.book_cover_content_description),
             modifier = Modifier.size(60.dp),
+            colorFilter = ColorFilter.tint(LocalContentColor.current),
         )
 
         Text(
