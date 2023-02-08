78fb50fc2a7113ebdc9825db84a785d99ef47295
Adam McNeilly <amcneilly331@gmail.com>
2023-02-08 14:04:12 -0500

    Adding images to library screen.

--- app/src/main/java/com/adammcneilly/reader/library/LibraryBookListItem.kt
+++ app/src/main/java/com/adammcneilly/reader/library/LibraryBookListItem.kt
@@ -1,12 +1,16 @@
 package com.adammcneilly.reader.library
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
 import com.adammcneilly.reader.displaymodels.BookDisplayModel
 import com.adammcneilly.reader.ui.theme.ReaderTheme
 
@@ -18,19 +22,33 @@ fun LibraryBookListItem(
     Card(
         modifier = modifier,
     ) {
-        Column(
+        AsyncImage(
+            model = displayModel.thumbnailURL,
+            contentDescription = "${displayModel.title} Cover",
             modifier = Modifier
-                .padding(ReaderTheme.sizing.listItemPadding),
-        ) {
-            Text(
-                text = displayModel.title,
-                style = MaterialTheme.typography.headlineSmall,
-            )
+                .fillMaxWidth()
+                .aspectRatio(0.75F),
+            contentScale = ContentScale.Crop,
+        )
 
-            Text(
-                text = displayModel.author,
-                style = MaterialTheme.typography.bodyMedium,
-            )
-        }
+        TitleAndAuthor(displayModel)
+    }
+}
+
+@Composable
+private fun TitleAndAuthor(displayModel: BookDisplayModel) {
+    Column(
+        modifier = Modifier
+            .padding(ReaderTheme.sizing.listItemPadding),
+    ) {
+        Text(
+            text = displayModel.title,
+            style = MaterialTheme.typography.headlineSmall,
+        )
+
+        Text(
+            text = displayModel.author,
+            style = MaterialTheme.typography.bodyMedium,
+        )
     }
 }
