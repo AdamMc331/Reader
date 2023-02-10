b26aad23f0c852373be055c10079476b246c2aac
Adam McNeilly <amcneilly331@gmail.com>
2023-02-09 21:58:05 -0500

    Adding distinct until changed check.

--- app/src/main/java/com/adammcneilly/reader/booksearch/BookSearchViewModel.kt
+++ app/src/main/java/com/adammcneilly/reader/booksearch/BookSearchViewModel.kt
@@ -9,6 +9,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
@@ -31,6 +32,7 @@ class BookSearchViewModel @Inject constructor(
     init {
         _viewState
             .map { it.searchText }
+            .distinctUntilChanged()
             .debounce(QUERY_DEBOUNCE_MILLIS)
             .onEach { searchText ->
                 val books = repository
