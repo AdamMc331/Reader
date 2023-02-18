211c43492df3c55b97853c43353cb859eb292420
Adam McNeilly <amcneilly331@gmail.com>
2023-02-18 15:40:50 -0500

    Removing unused top bar action types.

--- app/src/main/java/com/adammcneilly/reader/home/HomeConfig.kt
+++ app/src/main/java/com/adammcneilly/reader/home/HomeConfig.kt
@@ -3,17 +3,14 @@ package com.adammcneilly.reader.home
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import com.adammcneilly.reader.ui.ReaderNavigationType
-import com.adammcneilly.reader.ui.TopBarActionType
 
 /**
  * Defines the configurations for the home screen that may change based on window size. This class should
  * be generated using the [fromWindowSize] method.
  *
- * @property[appBarActionType] The [TopBarActionType] to render on the home screen.
  * @property[navigationType] The [ReaderNavigationType] used to navigate within the home screen.
  */
 data class HomeConfig(
-    val appBarActionType: TopBarActionType,
     val navigationType: ReaderNavigationType,
 ) {
 
@@ -23,28 +20,24 @@ data class HomeConfig(
                 WindowWidthSizeClass.Compact -> {
                     HomeConfig(
                         navigationType = ReaderNavigationType.BOTTOM_NAVIGATION,
-                        appBarActionType = TopBarActionType.ICON,
                     )
                 }
 
                 WindowWidthSizeClass.Medium -> {
                     HomeConfig(
                         navigationType = ReaderNavigationType.NAVIGATION_RAIL,
-                        appBarActionType = TopBarActionType.TEXT,
                     )
                 }
 
                 WindowWidthSizeClass.Expanded -> {
                     HomeConfig(
                         navigationType = ReaderNavigationType.PERMANENT_NAVIGATION_DRAWER,
-                        appBarActionType = TopBarActionType.TEXT,
                     )
                 }
 
                 else -> {
                     HomeConfig(
                         navigationType = ReaderNavigationType.BOTTOM_NAVIGATION,
-                        appBarActionType = TopBarActionType.ICON,
                     )
                 }
             }
--- app/src/main/java/com/adammcneilly/reader/home/HomeViewState.kt
+++ app/src/main/java/com/adammcneilly/reader/home/HomeViewState.kt
@@ -1,18 +1,14 @@
 package com.adammcneilly.reader.home
 
 import com.adammcneilly.reader.displaymodels.HomeNavigationTabDisplayModel
-import com.adammcneilly.reader.displaymodels.TopBarActionDisplayModel
 
 /**
  * Defines the UI configuration of the home screen at any given point in time.
  *
- * @property[topBarActions] A list of [TopBarActionDisplayModel] entities which are rendered in the top bar
- * of the home screen.
  * @property[navigationTabs] The list of [HomeNavigationTabDisplayModel] entities to show to the user.
  * @property[selectedTab] If defined, this is a tab that we need to navigate to. Otherwise, no navigation should happen.
  */
 data class HomeViewState(
-    val topBarActions: List<TopBarActionDisplayModel> = emptyList(),
     val navigationTabs: List<HomeNavigationTabDisplayModel> = emptyList(),
     val selectedTab: HomeNavigationTab? = null,
 )
