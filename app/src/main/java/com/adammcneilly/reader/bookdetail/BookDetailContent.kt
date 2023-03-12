dd80aaf57b8a32882b095d554117e6537fcce4d5
Adam McNeilly <amcneilly331@gmail.com>
2023-03-11 21:02:51 -0500

    Getting sheet type UI working.

--- app/src/main/java/com/adammcneilly/reader/bookdetail/BookDetailContent.kt
+++ app/src/main/java/com/adammcneilly/reader/bookdetail/BookDetailContent.kt
@@ -3,9 +3,12 @@ package com.adammcneilly.reader.bookdetail
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,29 +33,92 @@ fun BookDetailContent(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .padding(ReaderTheme.sizing.screenPadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(ReaderTheme.sizing.verticalListSpacing),
+            .background(MaterialTheme.colorScheme.primaryContainer),
     ) {
-        AsyncImage(
-            model = "https://books.google.com/books/content?id=yud-foXqGUEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            contentDescription = stringResource(R.string.book_cover_content_description),
+        Column(
             modifier = Modifier
-                .height(360.dp)
-                .clip(MaterialTheme.shapes.large),
-            contentScale = ContentScale.FillHeight,
-        )
+                .fillMaxWidth()
+                .padding(ReaderTheme.sizing.screenPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(ReaderTheme.sizing.verticalListSpacing),
+        ) {
+            AsyncImage(
+                model = "https://books.google.com/books/content?id=yud-foXqGUEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                contentDescription = stringResource(R.string.book_cover_content_description),
+                modifier = Modifier
+                    .height(360.dp)
+                    .clip(MaterialTheme.shapes.large),
+                contentScale = ContentScale.FillHeight,
+            )
 
-        Text(
-            text = viewState.book.title,
-            style = MaterialTheme.typography.headlineLarge,
-        )
+            Text(
+                text = viewState.book.title,
+                style = MaterialTheme.typography.headlineLarge,
+            )
 
-        Text(
-            text = viewState.book.author,
-            style = MaterialTheme.typography.titleLarge,
-        )
+            Text(
+                text = viewState.book.author,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = MaterialTheme.colorScheme.background,
+                    shape = RoundedCornerShape(
+                        topStart = 24.dp,
+                        topEnd = 24.dp,
+                    ),
+                )
+                .padding(16.dp),
+        ) {
+            Text(
+                text = "Book info",
+                style = MaterialTheme.typography.titleSmall,
+            )
+
+            Row {
+                Text(
+                    text = "Title",
+                    modifier = Modifier
+                        .weight(1F),
+                )
+
+                Text(
+                    text = "Leviathan Wakes",
+                )
+            }
+
+            Divider()
+
+            Row {
+                Text(
+                    text = "Authors",
+                    modifier = Modifier
+                        .weight(1F),
+                )
+
+                Text(
+                    text = "James S.A. Corey",
+                )
+            }
+
+            Divider()
+
+            Row {
+                Text(
+                    text = "Number of pages",
+                    modifier = Modifier
+                        .weight(1F),
+                )
+
+                Text(
+                    text = "534",
+                )
+            }
+        }
     }
 }
 
