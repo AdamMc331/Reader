b65fe6d3937b46a78fbd0378730cc66df7dd2977
Adam McNeilly <amcneilly331@gmail.com>
2025-05-23 00:21:20 -0400

    Renaming template.

--- app/src/main/java/com/adammcneilly/reader/ReaderApp.kt
+++ app/src/main/java/com/adammcneilly/reader/ReaderApp.kt
@@ -1,7 +1,7 @@
-package template
+package com.adammcneilly.reader
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TemplateApp : Application()
+class ReaderApp : Application()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "template"
+rootProject.name = "Reader"
 include(":app")
