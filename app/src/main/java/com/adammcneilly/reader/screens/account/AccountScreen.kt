488e7296eaf76668dfe46b0536e93aecaf050d6d
Adam McNeilly <amcneilly331@gmail.com>
2025-05-23 19:44:30 -0400

    Stubbing additional screens.

---
+++ app/src/main/java/com/adammcneilly/reader/screens/account/AccountScreen.kt
@@ -0,0 +1,14 @@
+package com.adammcneilly.reader.screens.account
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AccountScreen(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Stub Account Screen",
+    )
+}
---
+++ app/src/main/java/com/adammcneilly/reader/screens/menu/MenuScreen.kt
@@ -0,0 +1,14 @@
+package com.adammcneilly.reader.screens.menu
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MenuScreen(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Stub Menu Screen",
+    )
+}
