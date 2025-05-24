73e153aaddc3872451bc16a52a4581f58bfcd618
Adam McNeilly <amcneilly331@gmail.com>
2025-05-24 11:06:09 -0400

    Suppressing long parameter list.

--- app/src/main/java/com/adammcneilly/reader/core/ui/scaffold/PersistentScaffold.kt
+++ app/src/main/java/com/adammcneilly/reader/core/ui/scaffold/PersistentScaffold.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 
 @Composable
+@Suppress("LongParameterList")
 @OptIn(ExperimentalSharedTransitionApi::class)
 fun ScaffoldState.PersistentScaffold(
     modifier: Modifier = Modifier,
