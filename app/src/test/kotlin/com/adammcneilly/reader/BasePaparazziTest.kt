2b6f7587dda39e8a63b6a81635f22f9c7666f248
Adam McNeilly <amcneilly331@gmail.com>
2025-05-24 21:51:45 -0400

    Updating tests.

--- app/src/test/kotlin/com/adammcneilly/reader/BasePaparazziTest.kt
+++ app/src/test/kotlin/com/adammcneilly/reader/BasePaparazziTest.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.Paparazzi
+import com.adammcneilly.reader.core.app.LocalAppState
+import com.adammcneilly.reader.core.app.previewAppState
 import com.adammcneilly.reader.core.ui.theme.ReaderTheme
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
@@ -39,16 +42,20 @@ abstract class BasePaparazziTest {
                 darkTheme = useDarkTheme,
                 dynamicTheme = false,
             ) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize(),
+                CompositionLocalProvider(
+                    LocalAppState provides previewAppState,
                 ) {
-                    Box(
+                    Surface(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .padding(screenPaddingDp.dp),
+                            .fillMaxSize(),
                     ) {
-                        content()
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(screenPaddingDp.dp),
+                        ) {
+                            content()
+                        }
                     }
                 }
             }
--- app/src/test/kotlin/com/adammcneilly/reader/screens/search/SearchContentPaparazziTest.kt
+++ app/src/test/kotlin/com/adammcneilly/reader/screens/search/SearchContentPaparazziTest.kt
@@ -1,23 +1,40 @@
 package com.adammcneilly.reader.screens.search
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.text.input.TextFieldValue
 import com.adammcneilly.reader.BasePaparazziTest
 import com.adammcneilly.reader.R
+import com.adammcneilly.reader.core.app.AppState
+import com.adammcneilly.reader.core.app.AppStateData
+import com.adammcneilly.reader.core.app.HomeTab
+import com.adammcneilly.reader.core.app.LocalAppState
 import com.adammcneilly.reader.core.displaymodels.AuthorDisplayModel
 import com.adammcneilly.reader.core.displaymodels.BookDisplayModel
 import com.adammcneilly.reader.core.displaymodels.ImageDisplayModel
 import org.junit.Test
 
 class SearchContentPaparazziTest : BasePaparazziTest() {
+    private val searchAppState = AppState(
+        initialData = AppStateData(
+            selectedTab = HomeTab.Search,
+        ),
+    )
+
     @Test
     fun renderDefault() {
         snapshot(
             screenPaddingDp = 0,
         ) {
-            SearchContent(
-                state = SearchState.default(),
-                onEvent = {},
-            )
+            CompositionLocalProvider(
+                LocalAppState provides searchAppState,
+            ) {
+                SearchContent(
+                    state = SearchState.default(),
+                    onEvent = {},
+                    contentPadding = PaddingValues(),
+                )
+            }
         }
     }
 
@@ -26,24 +43,29 @@ class SearchContentPaparazziTest : BasePaparazziTest() {
         snapshot(
             screenPaddingDp = 0,
         ) {
-            SearchContent(
-                state = SearchState(
-                    query = TextFieldValue("Androids"),
-                    books = List(3) { index ->
-                        BookDisplayModel(
-                            id = index.toString(),
-                            title = "Androids",
-                            coverImage = ImageDisplayModel.Local(R.drawable.androids),
-                            author = AuthorDisplayModel(
-                                id = "123",
-                                name = "Chet Haase",
-                                image = ImageDisplayModel.Local(R.drawable.chet),
-                            ),
-                        )
-                    },
-                ),
-                onEvent = {},
-            )
+            CompositionLocalProvider(
+                LocalAppState provides searchAppState,
+            ) {
+                SearchContent(
+                    state = SearchState(
+                        query = TextFieldValue("Androids"),
+                        books = List(3) { index ->
+                            BookDisplayModel(
+                                id = index.toString(),
+                                title = "Androids",
+                                coverImage = ImageDisplayModel.Local(R.drawable.androids),
+                                author = AuthorDisplayModel(
+                                    id = "123",
+                                    name = "Chet Haase",
+                                    image = ImageDisplayModel.Local(R.drawable.chet),
+                                ),
+                            )
+                        },
+                    ),
+                    onEvent = {},
+                    contentPadding = PaddingValues(),
+                )
+            }
         }
     }
 }
